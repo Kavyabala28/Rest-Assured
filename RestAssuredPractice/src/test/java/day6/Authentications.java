a7f009a141d6393da6e091e599375908d7dc1279
Kavya B <161735556+Kavyabala28@users.noreply.github.com>
2025-01-15 19:20:23 +0530

    Add files via upload

---
+++ RestAssuredPractice/src/test/java/day6/Authentications.java
@@ -0,0 +1,28 @@
+package day6;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class Authentications {
+	
+	// Types of Authentications
+	
+	// 1. Basic 
+	@Test(priority = 1)
+	void testBasicAuthentication() {
+		
+		given()
+		     .auth().basic("postman", "password")
+		
+		.when()
+		     .get("https://postman-echo.com/basic-auth")
+		
+		.then()
+		      .statusCode(200)
+		      .body("authenticated", equalTo(true))
+		      .log().all();
+	}
+
+}
